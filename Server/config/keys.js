03cec32df51324822ce47f8be510fe2d18572b1f
senthur <senthur@financebuddha.com>
2018-03-27 01:36:34 +0530

    Optimized Site with new UI Changes,validations and also added new Client secrent Id from askskillerbot@gmail.com

--- Server/config/keys.js
+++ Server/config/keys.js
@@ -1,13 +1,15 @@
 //add to .gitignore
 
-
+//Previous Id
+//Client Id = 37483636987-l2uv4oa5dp76h9r3bo6eqoortk6f88ig.apps.googleusercontent.com
+// ClientSecret =80932UL5zQKweMX-6kFud0H8;
 module.exports = {
     mongodb:{
         dbURL:'mongodb://mongodb:test@ds161346.mlab.com:61346/linkedin-data'
     },
     google:{
-        clientID:'37483636987-l2uv4oa5dp76h9r3bo6eqoortk6f88ig.apps.googleusercontent.com',
-        clientSecret:'80932UL5zQKweMX-6kFud0H8'
+        clientID:'948827112863-q01l99bqnqb05ia7pig2038a080vne5g.apps.googleusercontent.com',                
+        clientSecret:'ASIKiRTjswAv__xuHTmbsYqb'
     },
     linkedIn:{
         clientID:'81osj02dqytkh8',
